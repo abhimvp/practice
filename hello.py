e2e45277540c4f4c103662e25d53b673e3d6c00d
abhishek <abhishekreddyboddu1@gmail.com>
2024-02-23 15:07:58 -0800

    test_vscode_git_setup

---
+++ hello.py
@@ -0,0 +1,5 @@
+message = "I have arrived"
+print(message)
+
+# Select one or more lines, then press Shift+Enter or right-click and select Run Selection/Line in Python Terminal. This command is convenient for testing just a part of a file.
+print("hello_world {0}".format(message))
